d7f03df78efc9d34b9bf51e4d4b132bf5cfb6632
Vlad.Zherko <vladikon19@tut.by>
2023-08-27 18:32:17 +0300

    Added history clients fully working save method for clients

--- src/main/java/comm/vlad/system/domain/CashMachine.java
+++ src/main/java/comm/vlad/system/domain/CashMachine.java
@@ -6,10 +6,11 @@ import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 @Data
 @AllArgsConstructor
@@ -17,14 +18,17 @@ public class CashMachine {
     /*private int allCash = 12_000;*/
     private Client client;
 
-    public void saveInfo(){
+    private void saveInfo(){
         System.out.println("Choose card :");
         for (int i = 0; i < client.getCards().size(); i++) {
             System.out.println(i + " " + client.getCards().get(i).getBanks());
         }
-        new Client().save(client);
+        int idCard = new Scanner(System.in).nextInt();
+        Client.save(client);
+        Card.save(client, client.getCards().get(idCard));
     }
     private int menu(){
+        saveInfo();
         String contextMenu = """
                 1. Sum on card
                 2. Transaction to another card
@@ -51,6 +55,29 @@ public class CashMachine {
             default -> {
                 System.out.println("Wrong operator");
             }
+        }
+    }
+
+    public void showHistory() {
+        List<String> arr = new ArrayList<>();
+        try (Statement statement = DataBaseConnectivity.getStatement();
+             ResultSet resultSet = statement.executeQuery(
+                     "SELECT name, surname, verNumber, title " +
+                             "FROM card " +
+                             "JOIN clients c on c.client_id = card.to_client " +
+                             "JOIN banks b on b.bank_id = card.id_bank;")) {
+            while(resultSet.next()){
+               StringJoiner stringBuilder = new StringJoiner(" ");
+               stringBuilder.add(resultSet.getString(1));
+               stringBuilder.add(resultSet.getString(2));
+               stringBuilder.add(resultSet.getString(3));
+               stringBuilder.add(resultSet.getString(4));
+               arr.add(String.valueOf(stringBuilder));
+            }
+            arr.forEach(System.out::println);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
